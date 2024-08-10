54d89c5c495e9d8602576552e13f2330926e97a1
Gustavo Hernandez <tavo16@gmail.com>
2024-08-10 22:55:58 +0200

    primera version multi-clase

---
+++ parts_segmentation/models/UnetModelMultiClass.py
@@ -0,0 +1,97 @@
+from torch.nn import functional as F
+from torch import nn, optim
+import torch 
+
+
+class Conv_3_k(nn.Module):
+    def __init__(self,channels_in,channels_out):
+        super().__init__()
+        self.conv1 = nn.Conv2d(channels_in,channels_out,kernel_size=3,stride=1,padding=1)
+
+    def forward(self,x):
+        return self.conv1(x)
+
+class Double_Conv(nn.Module):
+    '''Capas dobles de convoluciones para downsampling'''
+    def __init__(self, channels_in, channels_out):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+                            Conv_3_k(channels_in,channels_out),
+                            nn.BatchNorm2d(channels_out),
+                            nn.ReLU(),
+
+                            Conv_3_k(channels_out,channels_out),
+                            nn.BatchNorm2d(channels_out),
+                            nn.ReLU()
+        )
+    def forward(self, x):
+        return self.double_conv(x)
+
+class Down_conv(nn.Module):
+    '''
+    Downsampling
+    '''
+    def __init__(self, channels_in, channels_out):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.MaxPool2d(2,2),
+            Double_Conv(channels_in,channels_out)
+        )
+
+    def forward(self,x):
+        return self.encoder(x)
+    
+
+class Up_conv(nn.Module):
+    """Proceso inverson de subida. En vez de usar transpose conv layers, usamos upsample + 1x1 conv"""
+    def __init__(self,channels_in,channels_out):
+        super().__init__()
+        self.upsample_layer = nn.Sequential(
+                                nn.ConvTranspose2d(channels_in,channels_in//2,kernel_size=2,stride=2,padding=0)
+                                #nn.Conv2d(channels_in,channels_in//2, kernel_size=1,stride=1,padding=1)
+                                )
+        self.decoder = Double_Conv(channels_in,channels_out)
+
+    def forward(self,x1,x2):
+        """x1 es l feature map que viene de abajo 
+        x2 es el feature map que viene de la parte de downsampling (espejo)"""        
+        #print(x1.shape,x2.shape)
+        x1 = self.upsample_layer(x1)
+        #print(x1.shape)
+        x2 = x1[:,:,0:x1.size()[2],0: x1.size()[3]]
+        x = torch.cat([x2,x1],dim=1)
+        return self.decoder(x)
+
+
+class UNET(nn.Module):
+    """modelo total integrado"""
+    def __init__(self, channels_in, channels, num_classes):
+        super().__init__()
+        self.first_conv = Double_Conv(channels_in, channels)      # 64   224 x 224
+        self.down_conv1 = Down_conv(channels    , channels * 2)   # 128  112 x 112
+        self.down_conv2 = Down_conv(channels * 2, channels * 4)   # 256  56 X 56
+        self.down_conv3 = Down_conv(channels * 4, channels * 8)   # 512  28 X 28
+        self.middle_conv = Down_conv(channels * 8, channels * 16)  # 1024 14 x 14
+
+        self.up_conv1 = Up_conv(channels * 16, channels * 8)  # 1024 52 X52
+        self.up_conv2 = Up_conv(channels *  8, channels * 4)
+        self.up_conv3 = Up_conv(channels *  4, channels * 2)
+        self.up_conv4 = Up_conv(channels *  2, channels  )
+
+        self.last_conv = nn.Conv2d(channels, num_classes, kernel_size=1, stride= 1,padding =(0,2))
+        self.softmax = nn.Softmax(dim = 1)
+
+    def forward(self,x):
+        x1 = self.first_conv(x)
+        x2 = self.down_conv1(x1)
+        x3 = self.down_conv2(x2)
+        x4 = self.down_conv3(x3)
+
+        x5 = self.middle_conv(x4)
+
+        u1 = self.up_conv1(x5,x4)
+        u2 = self.up_conv2(u1,x3)
+        u3 = self.up_conv3(u2,x2)
+        u4 = self.up_conv4(u3,x1)
+        x = self.last_conv(u4)
+        return self.softmax(x)
---
+++ parts_segmentation/train.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import random
+import torch
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import transforms as T
+from torch.utils.data import DataLoader, random_split
+#
+import PIL
+from PIL import Image
+#
+from cargar_dataset import carga_carDataset
+from models.UnetModelMultiClass import *
+from utilidades.utilidades import finding_lr,accuracy, model_test
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+##Paths
+PATH = "data/"
+TRAIN_PATH = "data/train/JPEGImages/"
+TRAIN_MASKS_PATH="data/train/JPEGmasks/"
+TEST_PATH = "data/test/"
+#
+BATCH_SIZE = 32
+
+
+transform_data = T.Compose([
+        T.Resize([224,244]),
+        T.ToTensor()
+    ])
+
+
+full_dataset = carga_carDataset.Car_Dataset(
+    TRAIN_PATH,
+    TRAIN_MASKS_PATH,
+    img_transforms=transform_data,
+    mask_transforms=transform_data
+)
+
+TRAIN_SIZE = int(len(full_dataset)*0.8)
+VAL_SIZE = len(full_dataset) - TRAIN_SIZE
+
+train_dataset, val_dataset = random_split(full_dataset, [TRAIN_SIZE,VAL_SIZE])
+
+train_loader = DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True)
+val_loader   = DataLoader(val_dataset,batch_size=BATCH_SIZE,shuffle=True)
+
+#training
+
+def train(model, optimiser, criterion, scheduler = None, epochs = 100, store_every = 25):
+    model = model.to(device= device)
+    for epoch in range(epochs):
+        train_correct_num = 0   # se resetea cada epoch
+        train_total =  0   # num elementos evaluados
+        train_cost_acum = 0. #  coste acumulado
+        for mb , (x, y) in enumerate(train_loader, start=1):
+            model.train()
+            x= x.to(device=device, dtype = torch.float32)
+            y = y.to(device=device, dtype= torch.long).squeeze(1)  # eliminamos el canal 1 (blanco y negro)
+            _, targety, targetx = y.shape
+            scores  = model(x)
+            cost = criterion(input=scores, target=y)
+            optimiser.zero_grad()
+            cost.backward()
+            optimiser.step()
+            if scheduler: 
+                scheduler.step()
+            train_predictions = torch.argmax(scores, dim = 1)
+            train_correct_num += (train_predictions == y).sum()
+            train_total += torch.numel(train_predictions)
+            train_cost_acum += cost.item()
+            if mb%store_every == 0:
+                val_cost, val_acc, dice, iou = accuracy(model, val_loader,criterion)
+                train_acc = float(train_correct_num)/train_total
+                train_cost_every = float(train_cost_acum)/mb
+                print(f'mb: {mb}, train_cost: {train_cost_every:.3f}, val cost {val_cost:.3f},'
+                        f'train acc: {train_acc:.4f}, val acc: {val_acc:.3f}, dice: {dice}, iou: {iou}')
+                torch.save(model.state_dict(),'model_state_dict.pth')
+                torch.save(model,'entire_model_.pth')            
+
+def main():
+    epochs = 50
+    num_classes = 21
+    model = UNET(3,4,num_classes)
+    criterion = nn.CrossEntropyLoss()
+    try:
+        loaded_model = torch.load("entire_model_.pth")
+        model = loaded_model.eval()
+        print("existing model loaded")
+    except:
+        print("No se encontro el modelo")
+
+    optimiser_unet = torch.optim.Adam(model.parameters(),
+                                    lr = 1e-4
+                                    )
+    #esta funcion se usa para encontrar el lr ideal. pero no se usa mas.
+    #la simulacion mostro que el valor optimo es 0.1
+    #lg_lr, losses, accuracies  = finding_lr(model, optimiser_unet, train_loader, start_val=1e-6, end_val=10)
+
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimiser_unet,
+                                                    max_lr=0.1,
+                                                    steps_per_epoch=len(train_loader),
+                                                    epochs=epochs,pct_start=0.43,div_factor=10, final_div_factor=1000,
+                                                    three_phase=True
+                                                    )
+
+    #imagenes RGB de entrada
+    #solamente 4 canales, no los 64 de la red original para reducir coste computacional
+    # 2 clases para segmentacion
+
+
+    train(model, optimiser_unet, criterion, scheduler, epochs)
+
+#Train model 
+if __name__ == '__main__':
+    main()
